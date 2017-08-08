35f8d8defdb227ff338bee58bd3f911742f1bf8f
betrakiss <angelin.nedelchev@outlook.com>
2017-08-08 17:55:04 +0300

    Add logic to read mbox.

---
+++ src/MailCrawlerService/MboxReader.cs
@@ -0,0 +1,32 @@
+﻿using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MailCrawlerService
+{
+    public class MboxReader
+    {
+        public int Failed { get; private set; }
+        public List<MimeMessage> Read(string mboxFile)
+        {
+            List<MimeMessage> messages = new List<MimeMessage>();
+            FileStream stream = new FileStream(mboxFile, FileMode.Open);
+            var parser = new MimeParser(stream, MimeFormat.Mbox);
+            while (!parser.IsEndOfStream)
+            {
+                try
+                {
+                    messages.Add(parser.ParseMessage());
+                }
+                catch (Exception e)
+                {
+                    ++Failed;
+                }
+            }
+
+            return messages;
+        }
+    }
+}
