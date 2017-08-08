4340a22cf8804e950c25b8bc0a2ac798b002311b
betrakiss <angelin.nedelchev@outlook.com>
2017-08-08 17:56:10 +0300

    Add test logic.

--- src/MailCrawler.WPF/MailCrawler.WPF/MainWindow.xaml.cs
+++ src/MailCrawler.WPF/MailCrawler.WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MailCrawlerService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace MailCrawler.WPF
         public MainWindow()
         {
             InitializeComponent();
+            new ThunderbirdMailService().CrawlMail().ToList();
         }
     }
 }
---
+++ src/MailCrawlerService/MailClients.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailCrawlerService
+{
+    public enum MailClients
+    {
+        MissingNo = -1,
+        Outlook,
+        Thunderbird,
+        PostBox
+    }
+}
